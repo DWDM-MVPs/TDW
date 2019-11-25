c7a03c7d0bb749e65c98c695ff3a81279191c907
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-25 17:36:23 +0000

    Fim da ficha (não sei se funciona)

--- Programação Server-Side/ExpressJS_Project/ExpressJS_Project/index.js
+++ Programação Server-Side/ExpressJS_Project/ExpressJS_Project/index.js
@@ -13,5 +13,52 @@ app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
-app.listen(port, () => console.log("Aplicaï¿½ï¿½o de TDW - Miguel Campos"));
+app.listen(port, () => console.log("Aplicação de TDW - Miguel Campos"));
 // # < Load
+
+
+
+
+
+// # > Exercício
+let minhas_notas = [20, 10, 15, 17];
+
+
+
+app.get("/", (res) => {
+	res.send(minhas_notas);
+});
+
+app.get("/:id", (req, res) => {
+	console.log("GET: " + req.params.id);
+	var valor = minhas_notas[req.params.id];
+	if (valor != null) {
+		res.send(String(minhas_notas[req.params.id]));
+	}
+	else {
+		res.status(404).send("Registo não encontrado.");
+	}
+});
+
+app.post("/", (req, res) => {
+	console.log("POST: " + req.body.nota);
+	minhas_notas.push(parseInt(req.body.nota));
+	res.status(200).send("Registo adicionado.");
+});
+
+app.patch("/:id", (req, res) => {
+	console.log("PATCH:" + req.params.id + " | " + req.body.nota);
+	minhas_notas[req.params.id] = parseInt(req.body.nota);
+	res.status(200).send("Registo atualizado.");
+});
+
+app.delete('/:id', (req, res) => {
+	if (req.params.id != null) {
+		minhas_notas.splice(req.params.id);
+	}
+	else {
+		minhas_notas = null;
+		res.status(200)
+	}
+});
+// # < Exercício
